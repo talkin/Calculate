5cbc2d5e187fb7191ff8b4eebf9a69bfd9a939da
talkin <taojing1106@163.com>
2014-12-01 21:22:22 +0800

    [Jing] : ParkingLot - Add tests in SmartParkingBoy.

--- test/com/company/ParkingLot/SmartParkingBoyTest.java
+++ test/com/company/ParkingLot/SmartParkingBoyTest.java
@@ -1,5 +1,7 @@
 package com.company.ParkingLot;
 
+import com.company.ParkingLot.Exception.InvalidTicketException;
+import com.company.ParkingLot.Exception.NotAvailableException;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -23,5 +25,30 @@ public class SmartParkingBoyTest {
         ParkingLot parkingLot = smartParkingBoy.chooseParkingLot(parkingLots);
 
         assertThat(parkingLot, is(p2));
+    }
+
+    @Test(expected = InvalidTicketException.class)
+    public void should_get_exception_when_ticket_is_not_available() throws Exception {
+        parkingLots.add(new ParkingLot(3));
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        smartParkingBoy.getCar(new Ticket());
+    }
+
+    @Test
+    public void should_get_car_when_have_a_valid_ticket() throws Exception {
+        parkingLots.add(new ParkingLot(3));
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car car = new Car();
+        Ticket ticket = smartParkingBoy.park(car);
+        Car actualCar = smartParkingBoy.getCar(ticket);
+
+        assertThat(actualCar, is(car));
+    }
+
+    @Test(expected = NotAvailableException.class)
+    public void should_get_exception_when_all_parking_lots_is_not_available() throws Exception {
+        parkingLots.add(new ParkingLot(0));
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        smartParkingBoy.park(new Car());
     }
 }
